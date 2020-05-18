ac6a080fac4e35d5eb7f3b240f1f53e0146be874
Wayne Folkes <wfolkes@shutterstock.com>
2020-05-18 17:39:59 -0400

    print the date and cache the value

--- src/app.py
+++ src/app.py
@@ -5,37 +5,50 @@ from random import choice, choices, randint
 import sched
 import sys
 import time
+import datetime
+import calendar
 
 import aws_lambda_logging
+from cachetools import TTLCache, cached
 
 log = logging.getLogger()
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 INTERVALS = [1,5,10,15,30,60,90,120,150,180,240,300]
 EXCEPTION_TYPES = ['TypeError', 'NameError','ModuleNotFoundError','DatabaseNotFoundError','ZeroDivisionError']
+day_cache = TTLCache(maxsize=1000, ttl=3600)
+
+@cached(day_cache)
+def find_day():
+    day_num = datetime.datetime.today().weekday()
+    return (calendar.day_name[day_num])
 
 def make_errors():
+    day = find_day()
     for n in range(0,choice(INTERVALS)):
         for exception in choices(EXCEPTION_TYPES,k=randint(1,len(EXCEPTION_TYPES))):
-            log.error("{}: Uh oh something is wrong".format(exception))
+            log.error("{}: Uh oh something is wrong - Today is {}".format(exception,day))
         time.sleep(1/choice(INTERVALS))
 
 def make_warns():
+    day = find_day()
     for n in range(0,choice(INTERVALS)):
         for exception in choices(EXCEPTION_TYPES,k=randint(1,len(EXCEPTION_TYPES))):
-            log.warn("{}: This is a warn".format(exception))
-            log.warning("{}: This is a warning".format(exception))
+            log.warn("{}: This is a warn - Today is {}".format(exception,day))
+            log.warning("{}: This is a warning - Today is {}".format(exception,day))
         time.sleep(1/choice(INTERVALS))
 
 def make_fatals():
+    day = find_day()
     for n in range(0,choice(INTERVALS)):
         for exception in choices(EXCEPTION_TYPES,k=randint(1,len(EXCEPTION_TYPES))):
-            log.fatal("{}: This is fatal".format(exception))
+            log.fatal("{}: This is fatal - Today is {}".format(exception,day))
         time.sleep(1/choice(INTERVALS))
 
 def make_info():
+    day = find_day()
     for n in range(0,choice(INTERVALS)):
-        log.info("This is fine")
+        log.info("Today is {} and this is fine".format(day))
         time.sleep(0.25)
 
 def main():

9e3a735ceb0eca8fc7d1c153d63b61ce937da67e
Wayne Folkes <wfolkes@shutterstock.com>
2020-06-22 10:08:04 -0400

    making the linter a little happier

--- src/app.py
+++ src/app.py
@@ -15,40 +15,41 @@ from cachetools import TTLCache, cached
 log = logging.getLogger()
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-INTERVALS = [1,5,10,15,30,60,90,120,150,180,240,300]
-EXCEPTION_TYPES = ['TypeError', 'NameError','ModuleNotFoundError','DatabaseNotFoundError','ZeroDivisionError']
+INTERVALS = [1, 5, 10, 15, 30, 60, 90, 120, 150, 180, 240, 300]
+EXCEPTION_TYPES = ['TypeError', 'NameError', 'ModuleNotFoundError',
+                   'DatabaseNotFoundError', 'ZeroDivisionError']
 day_cache = TTLCache(maxsize=1000, ttl=3600)
 
 @cached(day_cache)
 def find_day():
     day_num = datetime.datetime.today().weekday()
-    return (calendar.day_name[day_num])
+    return calendar.day_name[day_num]
 
 def make_errors():
     day = find_day()
-    for n in range(0,choice(INTERVALS)):
-        for exception in choices(EXCEPTION_TYPES,k=randint(1,len(EXCEPTION_TYPES))):
-            log.error("{}: Uh oh something is wrong - Today is {}".format(exception,day))
+    for _ in range(0, choice(INTERVALS)):
+        for exception in choices(EXCEPTION_TYPES, k=randint(1, len(EXCEPTION_TYPES))):
+            log.error(f"{exception}: Uh oh something is wrong - Today is {day}")
         time.sleep(1/choice(INTERVALS))
 
 def make_warns():
     day = find_day()
-    for n in range(0,choice(INTERVALS)):
-        for exception in choices(EXCEPTION_TYPES,k=randint(1,len(EXCEPTION_TYPES))):
-            log.warn("{}: This is a warn - Today is {}".format(exception,day))
-            log.warning("{}: This is a warning - Today is {}".format(exception,day))
+    for _ in range(0, choice(INTERVALS)):
+        for exception in choices(EXCEPTION_TYPES, k=randint(1, len(EXCEPTION_TYPES))):
+            log.warn("{}: This is a warn - Today is {}".format(exception, day))
+            log.warning("{}: This is a warning - Today is {}".format(exception, day))
         time.sleep(1/choice(INTERVALS))
 
 def make_fatals():
     day = find_day()
-    for n in range(0,choice(INTERVALS)):
-        for exception in choices(EXCEPTION_TYPES,k=randint(1,len(EXCEPTION_TYPES))):
-            log.fatal("{}: This is fatal - Today is {}".format(exception,day))
+    for _ in range(0, choice(INTERVALS)):
+        for exception in choices(EXCEPTION_TYPES, k=randint(1, len(EXCEPTION_TYPES))):
+            log.fatal("{}: This is fatal - Today is {}".format(exception, day))
         time.sleep(1/choice(INTERVALS))
 
 def make_info():
     day = find_day()
-    for n in range(0,choice(INTERVALS)):
+    for _ in range(0, choice(INTERVALS)):
         log.info("Today is {} and this is fine".format(day))
         time.sleep(0.25)
 
@@ -57,15 +58,15 @@ def main():
         level=os.environ.get('LOGLEVEL', 'INFO'),
         boto_level='CRITICAL'
     )
-    methods = [make_errors,make_warns,make_fatals,make_info]
-    s = sched.scheduler(time.time, time.sleep)
+    methods = [make_errors, make_warns, make_fatals, make_info]
+    scheduler = sched.scheduler(time.time, time.sleep)
 
     while True:
         try:
-            s.enter(0.5,1,choice(methods))
-            s.enter(0.5,1,choice(methods))
-            s.run()
-        except (KeyboardInterrupt, SystemExit) as e:
+            scheduler.enter(0.5, 1, choice(methods))
+            scheduler.enter(0.5, 1, choice(methods))
+            scheduler.run()
+        except (KeyboardInterrupt, SystemExit):
             break
 
 if __name__ == "__main__":
